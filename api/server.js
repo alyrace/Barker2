b5ac249923e3b597b081f89d1a342e2af491706d
alyrace <alysonrace@gmail.com>
2021-09-09 11:42:07 -0700

    connected to db

--- api/server.js
+++ api/server.js
@@ -1,1 +1,12 @@
-git 
+const express = require('express');
+const connectDB = require("./config/db");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+
+app.listen(PORT, () =>{
+    console.log(`Listening on port ${PORT} ...`)
+});
